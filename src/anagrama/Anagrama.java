3ea3702137f8edcad9fd7080dc661b62af9878a2
Gustavo <gsoter@live.com>
2019-09-18 18:04:59 -0300

    v1.1

--- src/anagrama/Anagrama.java
+++ src/anagrama/Anagrama.java
@@ -1,11 +1,30 @@
 package anagrama;
 
-public class Anagrama implements IAnagrama{
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Anagrama implements IAnagrama {
 
 	@Override
 	public boolean isAnagramas(String s1, String s2) {
-		// TODO Auto-generated method stub
+		String str1 = s1.replaceAll(" ", "");
+		String str2 = s2.replaceAll(" ", "");
+		
+		if (!(numFinder(s1) || numFinder(s2)))
+		if (str1.length() == str2.length()) {
+			char[] array1 = str1.toLowerCase().toCharArray();
+			char[] array2 = str2.toLowerCase().toCharArray();
+			Arrays.sort(array1);
+			Arrays.sort(array2);
+			return (Arrays.equals(array1, array2));
+		}
 		return false;
 	}
 
+	public boolean numFinder(String s) {
+		Pattern p = Pattern.compile("\\d+");
+		Matcher m = p.matcher(s);
+		return m.find();
+	}
 }
